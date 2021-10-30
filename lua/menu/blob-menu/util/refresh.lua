0e6accaca013a3f0b9320a25e2ff73ac51e97d87
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2021-10-30 16:43:16 -0700

    feat: added watch reset command

--- lua/menu/blob-menu/util/refresh.lua
+++ lua/menu/blob-menu/util/refresh.lua
@@ -21,6 +21,12 @@ concommand.Add("blob_watch", function(_,_,a)
         _p(towatch)
         return
     end
+    if a[1] == "reset" then
+        print("[BlobMenu] [Watch] Stopped Watching all")
+        for k,v in pairs(towatch) do
+            towatch[k] = nil
+        end
+    end
     for k,v in pairs(a) do
         v = "menu/blob-menu/" .. v
         if towatch[v] then
@@ -38,13 +44,12 @@ hook.Add("DrawOverlay", "Menu:Watch", function()
         t = CurTime() + 3
 
         for k,v in pairs(towatch) do
-            file.AsyncRead(k, "LuaMenu", function(f, g, s, dat)
-                if dat ~= v then
-                    towatch[k] = r
-                    print("[BlobMenu] [Watch] Detected File Change, Refreshing!")
-                    RunConsoleCommand("blob_menu_refresh")
-                end
-            end )
+            local r = file.Read(k, "LuaMenu")
+            if r ~= v then
+                towatch[k] = r
+                print("[BlobMenu] [Watch] Detected File Change, Refreshing!")
+                RunConsoleCommand("blob_menu_refresh")
+            end
         end
     end
 end )
