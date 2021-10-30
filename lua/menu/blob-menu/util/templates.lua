136ce6162b16ed3fa360c38da619b79f3049029b
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2021-10-30 16:43:28 -0700

    feat: added broken for loop to templates

--- lua/menu/blob-menu/util/templates.lua
+++ lua/menu/blob-menu/util/templates.lua
@@ -1,4 +1,12 @@
 
+--[[
+    The status of this is... its a complete mess
+
+    It really needs to do one thing and it does it well but i made it do a bunch of other stuff and most of it doesnt work
+    Dont use if or for pls
+    Just use {{val}} <: ok bye bye
+]]
+
 menu = menu or {}
 menu.templates = menu.templates or {}
 
@@ -48,7 +56,7 @@ function menu.templates.EvalValue(toeval, vals)
     local split = string.Split(toeval, ".")
 
     if #split == 1 then
-        return tostring(vals[split[1]])
+        return vals[split[1]]
     end
 
     local down = vals[tonumber(split[1]) or split[1]]
@@ -59,16 +67,50 @@ function menu.templates.EvalValue(toeval, vals)
     return down
 end
 
+function menu.templates.EvalLoop(toeval, vals)
+    local split = string.Split(toeval:Trim(), "\n")
+    local cur = {
+        lines = "",
+        varname = false,
+        expr = false
+    }
+
+    for k,v in ipairs(split) do
+        local tr = v:Trim()
+        if tr:StartWith("for") then
+            local varname, expr = string.match(tr, "for%s?([^%s]+) in ([^%:]+):")
+            cur.varname = varname
+            cur.expr = expr
+        else
+            cur.lines = cur.lines .. "\n" .. tr
+        end
+    end
+
+    local built = ""
+    for k,v in pairs(menu.templates.EvalValue(cur.expr, vals)) do
+        vals[cur.varname] = v
+        built = built .. menu.templates.Render(cur.lines, vals)
+    end
+
+    return built
+end
+
 function menu.templates.Eval(toeval, vals)
     if toeval:Trim():StartWith("if") then
         return menu.templates.EvalIf(toeval, vals)
+    end
+
+    if toeval:Trim():StartWith("for") then
+        return menu.templates.EvalLoop(toeval, vals)
     end
 
     return tostring(menu.templates.EvalValue(toeval, vals))
 end
 
 function menu.templates.Render(str, values)
-    local x = string.gsub(str, "{{(.-)}}", function(toeval)
+    local x = str:gsub("{%%(.-)%%}", function(toeval)
+        return menu.templates.Eval(toeval, values)
+    end ):gsub("{{(.-)}}", function(toeval)
         return menu.templates.Eval(toeval, values)
     end )
     return x
