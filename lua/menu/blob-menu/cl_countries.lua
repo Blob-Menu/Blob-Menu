9d6dee3afd66c43db668e4a6481a717e1d463763
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2021-10-30 18:04:10 -0700

    fix: disabled country flags cause theyre awful

--- lua/menu/blob-menu/cl_countries.lua
+++ lua/menu/blob-menu/cl_countries.lua
@@ -38,68 +38,68 @@ menu.country_flags = {}
 
 -- YEAA BABY LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 -- AMERICA WOOOOOOOOOO YEAAAAAAAAAAAAAAAAAAHHHHHHHHHHH LETS GOOOO
-menu.country_flags.us = function(w, h)
-    draw.NoTexture()
-    surface.SetDrawColor(255, 255, 255)
+-- menu.country_flags.us = function(w, h)
+--     draw.NoTexture()
+--     surface.SetDrawColor(255, 255, 255)
 
-    local amt = 4
-    local hh = h / amt
-    for i = 0, amt do
-        if i % 2 == 0 then
-            surface.SetDrawColor(255, 57, 57)
-        else
-            surface.SetDrawColor(255,255,255)
-        end
+--     local amt = 4
+--     local hh = h / amt
+--     for i = 0, amt do
+--         if i % 2 == 0 then
+--             surface.SetDrawColor(255, 57, 57)
+--         else
+--             surface.SetDrawColor(255,255,255)
+--         end
 
-        local y = hh * i
-        surface.DrawPoly({
-            {x = 0, y = y},
-            {x = 0, y = y - hh},
-            {x = w, y = y},
-            {x = w, y = y + hh}
-        })
-    end
+--         local y = hh * i
+--         surface.DrawPoly({
+--             {x = 0, y = y},
+--             {x = 0, y = y - hh},
+--             {x = w, y = y},
+--             {x = w, y = y + hh}
+--         })
+--     end
 
-    surface.SetDrawColor(74, 74, 255)
-    surface.DrawPoly({
-        {x = 0, y = h},
-        {x = 0, y = 0},
-        {x = w, y = 0}
-    })
-end
+--     surface.SetDrawColor(74, 74, 255)
+--     surface.DrawPoly({
+--         {x = 0, y = h},
+--         {x = 0, y = 0},
+--         {x = w, y = 0}
+--     })
+-- end
 
-menu.country_flags.ca = function(w, h)
-    draw.NoTexture()
-    surface.SetDrawColor(255, 57, 57)
-    surface.DrawRect(0, 0, w, h / 4)
+-- menu.country_flags.ca = function(w, h)
+--     draw.NoTexture()
+--     surface.SetDrawColor(255, 57, 57)
+--     surface.DrawRect(0, 0, w, h / 4)
 
-    surface.SetDrawColor(255, 255, 255)
-    surface.DrawRect(0, h / 4, w, h / 2)
+--     surface.SetDrawColor(255, 255, 255)
+--     surface.DrawRect(0, h / 4, w, h / 2)
 
-    surface.SetDrawColor(255, 57, 57)
-    surface.DrawRect(0, h - h / 4, w, h / 4)
-end
+--     surface.SetDrawColor(255, 57, 57)
+--     surface.DrawRect(0, h - h / 4, w, h / 4)
+-- end
 
-menu.country_flags.ru = function(w, h)
-    draw.NoTexture()
-    surface.SetDrawColor(255, 255, 255)
-    surface.DrawRect(0, 0, w, h / 3)
+-- menu.country_flags.ru = function(w, h)
+--     draw.NoTexture()
+--     surface.SetDrawColor(255, 255, 255)
+--     surface.DrawRect(0, 0, w, h / 3)
 
-    surface.SetDrawColor(74, 74, 255)
-    surface.DrawRect(0, h / 3, w, h / 3)
+--     surface.SetDrawColor(74, 74, 255)
+--     surface.DrawRect(0, h / 3, w, h / 3)
 
-    surface.SetDrawColor(255, 57, 57)
-    surface.DrawRect(0, h - h / 3, w, h / 3)
-end
+--     surface.SetDrawColor(255, 57, 57)
+--     surface.DrawRect(0, h - h / 3, w, h / 3)
+-- end
 
-menu.country_flags.fr = function(w, h)
-    draw.NoTexture()
-    surface.SetDrawColor(74, 74, 255)
-    surface.DrawRect(0, 0, w / 3, h)
+-- menu.country_flags.fr = function(w, h)
+--     draw.NoTexture()
+--     surface.SetDrawColor(74, 74, 255)
+--     surface.DrawRect(0, 0, w / 3, h)
 
-    surface.SetDrawColor(255, 255, 255)
-    surface.DrawRect(w / 3, 0, w / 3, h)
+--     surface.SetDrawColor(255, 255, 255)
+--     surface.DrawRect(w / 3, 0, w / 3, h)
 
-    surface.SetDrawColor(255, 57, 57)
-    surface.DrawRect(w / 3 * 2, 0, w / 3, h)
-end
+--     surface.SetDrawColor(255, 57, 57)
+--     surface.DrawRect(w / 3 * 2, 0, w / 3, h)
+-- end
